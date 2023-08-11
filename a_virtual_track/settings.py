b209f87d2ecd8a7b2d168c8651ebd76be24bd411
minow3 <minow3@gmail.com>
2023-08-11 17:06:34 +0100

    CREATED: base.html with view and url

--- a_virtual_track/settings.py
+++ a_virtual_track/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://virtualtrack-97152dcafef4.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['https://virtualtrack-97152dcafef4.herokuapp.com/', 'localhost', '127.0.0.1:8000', '127.0.0.1']
 
 
 # Application definition
