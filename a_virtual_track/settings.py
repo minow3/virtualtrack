bf35188ed7e460b6f79affef84d18ec784d37d8a
minow3 <minow3@gmail.com>
2023-08-13 11:11:12 +0100

    FEAT: installed summernote

--- a_virtual_track/settings.py
+++ a_virtual_track/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'b_home',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
--- a_virtual_track/urls.py
+++ a_virtual_track/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('b_home.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
