f9dd45cda921cd9c02f6261b80b1eceb81b82179
minow3 <minow3@gmail.com>
2023-08-13 12:59:15 +0100

    FEAT: Created category model and assigned to admin section

--- b_home/admin.py
+++ b_home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
@@ -10,3 +10,10 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'slug')
+    search_fields = ['name']
+    prepopulated_fields = {'slug': ('name',)}
