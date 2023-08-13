9fb623909e132371d243e4d2d03c027f48ed81bd
minow3 <minow3@gmail.com>
2023-08-13 11:56:25 +0100

    FEAT: post model created

--- b_home/models.py
+++ b_home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django_summernote.fields import SummernoteTextField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Approved"))
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    content = SummernoteTextField()
+    image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
