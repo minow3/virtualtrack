7afaefc4601fd6a40ddf84e7714057bec6635055
minow3 <minow3@gmail.com>
2023-08-24 11:18:15 +0100

    heroku: deploy attmempt

--- a_virtual_track/settings.py
+++ a_virtual_track/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['virtualtrack-97152dcafef4.herokuapp.com', 'localhost', '127.0.0.1:8000', '127.0.0.1']
 
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
